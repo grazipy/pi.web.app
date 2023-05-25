2666bf38a7e76e42048a1e19333bc74d68298bd6
Alex da Silva <1806596@aluno.univesp.br>
2023-05-24 23:09:49 -0300

    V1.2

--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('licitagil/', include('licitagil.urls')),
+    path('', RedirectView.as_view(url='/licitagil/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
